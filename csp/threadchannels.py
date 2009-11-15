d17e53e83e0a41b60909b9eb2fabf18d858a5193
mount.sarah <devnull@localhost>
2009-11-15 11:27:41 +0000

    Added GPL notice to channel modules.

--- csp/threadchannels.py
+++ csp/threadchannels.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python
+
+"""
+Guards and channel types for the thread version of the CSP library.
+
+Copyright (C) Sarah Mount, 2009.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have rceeived a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+"""
+
 __author__ = 'Sarah Mount <s.mount@wlv.ac.uk>'
 __date__ = 'June 2009'
 
