a9e4d68b73740c66e6a1b98d3ee138f25ff93f08
mount.sarah <devnull@localhost>
2010-05-02 18:03:28 +0100

    Argh! Forgot to add csp.builtins to repository.

---
+++ csp/patterns.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""Design pattern support for python-csp.
+
+Copyright (C) Sarah Mount, 2010.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A ParTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have rceeived a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+"""
+
+
+__author__ = 'Sarah Mount <s.mount@wlv.ac.uk>'
+__date__ = 'May 2010'
+
+
+import os
+if os.environ.has_key('CSP'):
+    if os.environ['CSP'] == 'PROCESSES':
+        from csp.cspprocess import *
+    elif os.environ['CSP'] == 'THREADS':
+        from csp.cspthread import *
+else:
+    from csp.cspprocess import *   
+del os
+
+
+
+class TokenRing(Par):
+    def __init__(self, func, size, numtoks=1):
+        self.chans = [Channel() for channel in xrange(size)]
+        self.procs = [func(index=i,
+                           tokens=numtoks,
+                           numnodes=size,
+                           inchan=self.chans[i-1],
+                           outchan=self.chans[i]) for i in xrange(size)]
+        super(TokenRing, self).__init__(*self.procs) 
+        return
+
