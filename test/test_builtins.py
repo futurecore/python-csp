03670dc7b07386dbb5d4981567eb0336bf3268b8
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2010-07-23 20:33:13 +0100

    Provide spare channels. Poison channels to avoid blocking.

--- test/test_builtins.py
+++ test/test_builtins.py
@@ -19,14 +19,22 @@ import csp.builtins as builtins
 class TestBuiltinsWithProcesses(unittest.TestCase):
     csp_process = csp.cspprocess
 
-    def feed_builtin(self, in_data, builtin):
+    def setUp(self):
+        csp = self.csp_process
+        # get us some channels for later use
+        self.spare_channels = [csp.Channel() for i in xrange(3)]
+
+    def tearDown(self):
+        # ignore result
+        [channel.poison() for channel in self.spare_channels]
+
+    def feedBuiltin(self, in_data, builtin):
         """Feed the data from `in_data` into the builtin CSPProcess
         (process/thread) and return a sequence of the corresponding
         output values.
         """
         csp = self.csp_process
-        in_channel = csp.Channel()
-        out_channel = csp.Channel()
+        in_channel, out_channel = self.spare_channels[:2]
         started_builtin = builtin(in_channel, out_channel).start()
         out_data = []
         for data_item in in_data:
@@ -35,8 +43,9 @@ class TestBuiltinsWithProcesses(unittest.TestCase):
         return out_data
 
     def test_sin(self):
-        print self.feed_builtin([0, 0.1], builtins.Sin)
-
+        out_data = self.feed_builtin([0, 0.1], builtins.Sin)
+        print out_data
+        
 
 # class TestBuiltinsWithThreads(unittest.TestCase):
 #     csp_process = csp.cspthread
