a34ee712e968b22be1c3ab9f03cb36b4ef91c997
mount.sarah <devnull@localhost>
2010-07-27 23:46:23 +0100

    Added test for repetition; files for part08 of tutorial.

---
+++ test/testrep.py
@@ -0,0 +1,17 @@
+import sys
+
+sys.path.insert(0, "..")
+
+from csp.csp import *
+from csp.guards import Timer
+
+@process
+def hello():
+    t = Timer()
+    for i in xrange(5):
+        print i
+        t.sleep(1)
+
+if __name__ == '__main__':
+    hello() * 3
+    2 * hello()
